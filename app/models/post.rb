0cd692b42c51facef6a6e6c8cfd306878fcd1895
Dan Norman <denys.norman.tr.@gmail.com>
2016-11-08 18:26:32 +0200

    blog without style

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  validates :title, presence: true, length: {maximum: 10}
+  validates :content, presence: true, length: {maximum: 200}
+
+  belongs_to :user
+end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+ validates :name, presence: true, length: { minimum: 3, maximum: 10 },
+                                  uniqueness: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts
+end
