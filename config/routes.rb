2b88de290569945dbea2af166308f6eb0cf582fa
Dan Norman <denys.norman.tr.@gmail.com>
2016-11-09 00:17:08 +0200

    add style to header

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :posts
   root 'posts#index'
+  get  '/help',    to: 'static_pages#help'
 end
