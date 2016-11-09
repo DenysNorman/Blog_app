683b94041d9b01ad7de9038df7977a2a0d306fa2
Dan Norman <denys.norman.tr.@gmail.com>
2016-11-09 02:19:55 +0200

    add home link to header and add style to sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :posts
   root 'posts#index'
-  get  '/help',    to: 'static_pages#help'
 end
