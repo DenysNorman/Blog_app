80b18b8c992a6bd0b3ffc855659bbd8150d9dfc9
Dan Norman <denys.norman.tr.@gmail.com>
2016-12-07 22:15:46 +0200

    test accets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_filter :set_visitor_cookie
+  before_action :set_visitor_cookie
 
   def baner
   end
